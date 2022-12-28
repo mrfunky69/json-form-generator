5d478f3249c842b5ff0b2a2887e9d020f6ae784d
mrfunky69 <87094553+mrfunky69@users.noreply.github.com>
2022-12-28 22:34:35 +0530

    removed_unwanted_module

--- src/components/node_wrapper.js
+++ src/components/node_wrapper.js
@@ -4,7 +4,7 @@ import '../style.css'
 
 export default function NodeWrapper({ children, showAdvanceOptions, setShowAdvanceOptions, showAdvanceOptionButton }) {
     return (
-        <div style={{ border: '1px solid black' }}>
+        <div style={{ border: '1px solid #f1f7fe', borderRadius:'5px',backgroundColor:'#fbfbfe'}}>
             {children}
             {showAdvanceOptionButton ?
                 <div className='advance_option'>
